01f69513ba971f2fe651992603e99ab61156629e
ddock4you <ddock4you@gmail.com>
2020-10-17 13:28:21 +0900

    - login_jion_boilerplate 시작

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+mongoose
+    .connect(
+        "mongodb+srv://ddock4you:tmdgus123!@cluster0.wlayg.mongodb.net/boilerplate?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        }
+    )
+    .then(() => console.log("MongoDB Conencted"))
+    .catch((err) => console.log(err));
+
+// mongodb+srv://ddock4you:<password>@cluster0.wlayg.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+app.get("/", (req, res) => res.send("하이염"));
+
+app.listen(port, () => console.log(`server on port ${port}`));
