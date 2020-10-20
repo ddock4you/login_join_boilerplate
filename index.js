d6dd122ba7eb46f7278b120bcfbaab59ff8f0e6b
ddock4you <ddock4you@gmail.com>
2020-10-20 18:09:41 +0900

    - 로그인 기능 추가 중

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const config = require("./config/key");
+const cookieParser = require("cookie-parser");
 
 const { User } = require("./models/Users")
 
@@ -9,6 +10,7 @@ const { User } = require("./models/Users")
 app.use(express.urlencoded({ extended : false }));
 
 app.use(express.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -27,7 +29,7 @@ mongoose
 app.get("/", (req, res) => res.send("하이염!"));
 app.post("/register", (req, res) => {
     // 회원 가입할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
-    console.log(req.body);
+    // console.log(req.body);
     const user = new User(req.body);
     
     user.save((err, doc) => {
@@ -38,6 +40,32 @@ app.post("/register", (req, res) => {
     });
 });
 
+app.post("/login", (req, res) => {
+    // 요청된 이메일을 데이터베이스에서 찾는다.
+    User.findOne({ email: req.body.email}, (err, user) => {
+        if (!user) {
+            return res.json({
+                login: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            });
+        }
+        
+        // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 체크
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+        })
+        
+        // 비밀번호까지 맞다면 토큰을 새성하기.
+        user.generateToken((err, user) => {
+            if(err) return res.status(400).send(err);
+
+            res.cookie("x_auth", user.token).status(200).json({ loginSuccess: true, userId: user._id });            
+        });
+
+    });
+    
+
+});
 
 app.listen(port, () => console.log(`server on port ${port}`));
 
--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
     name: {
@@ -26,6 +29,46 @@ const userSchema = mongoose.Schema({
         type: Number,
     }
 });
+
+// arrow function과 일반 function의 차이를 느낄 수 있는 상황
+// https://poiemaweb.com/es6-arrow-function
+userSchema.pre('save', function (next) {
+    const user = this;
+    // console.log({user});
+    if(user.isModified("password")) {
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if (err) return next(err);
+    
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    // plainPassword 1234567 암호화된 비밀번호
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if (err) return cb(rrr);
+        cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generationToken = function(cb) {
+    // jsonwebtoken을 이용해서 토큰 생성하기
+    const user = this;
+    const token = jwt.sign(user.id, "secretToken");
+    user.token = token;
+    user.save(function(err, user){
+        if(err) return cb(err);
+        cb(null, user);
+    })
+}
+
 
 const User = mongoose.model('User', userSchema);
 
