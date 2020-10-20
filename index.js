e3841870568b4d04e395a3097cbf6157b69693ad
ddock4you <ddock4you@codeline.kr>
2020-10-20 15:36:43 +0900

    - nodemon 설치 - 개발, 배포 환경에 따라 db정보 다르게 불러오게 설정

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const config = require("./config/key");
+
+const { User } = require("./models/Users")
+
+// application/x-www-form-urlencoded 데이터를 가져와서 분석
+app.use(express.urlencoded({ extended : false }));
+
+app.use(express.json());
 
 const mongoose = require("mongoose");
 mongoose
     .connect(
-        "mongodb+srv://ddock4you:tmdgus123!@cluster0.wlayg.mongodb.net/boilerplate?retryWrites=true&w=majority",
+        config.mongoURI,
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
@@ -16,6 +24,19 @@ mongoose
     .then(() => console.log("MongoDB Conencted"))
     .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("하이염"));
+app.get("/", (req, res) => res.send("하이염!"));
+app.post("/register", (req, res) => {
+    // 회원 가입할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+    console.log(req.body);
+    const user = new User(req.body);
+    
+    user.save((err, doc) => {
+        if(err) return res.json({ success: false, err});
+        return res.status(200).json({
+            success: true
+        });
+    });
+});
+
 
 app.listen(port, () => console.log(`server on port ${port}`));
